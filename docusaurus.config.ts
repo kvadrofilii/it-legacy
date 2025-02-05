c9a2287c4254ec1d1941347477a22201fff581e8
Michael Yakovlev <kvadrofilii@gmail.com>
2025-02-05 22:31:59 +0300

    feat: Добавил подсветку синтаксиса bash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,6 +79,7 @@ const config: Config = {
         prism: {
             theme: prismThemes.github,
             darkTheme: prismThemes.dracula,
+            additionalLanguages: ['bash'],
         },
     } satisfies Preset.ThemeConfig,
 };
